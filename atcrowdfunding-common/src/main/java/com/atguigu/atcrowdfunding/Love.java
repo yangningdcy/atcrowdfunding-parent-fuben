cea0a976552fbf900f00236bbe984610e7ac3550
杨宁 <107954102@qq.com>
2020-08-09 12:46:52 +0800

    ~~~~~~~~dcy1~~~~~~~~~~

--- atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/Love.java
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/Love.java
@@ -4,5 +4,6 @@ public class Love {
     public static void main(String[] args) {
         System.out.println("1111master");
         System.out.println("1111dev");
+        System.out.println("~~~~~~~~~~~~~dcy1~~~~~~~~~~~~");
     }
 }
